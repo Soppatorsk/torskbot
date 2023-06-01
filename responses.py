5a8e6169a09e87ff1cd5a9bdc493d49b6d2f837e
Simon Karlsson <simon.kevlarsjal@gmail.com>
2023-06-01 02:09:16 +0200

    gif response, davinci to gtp3.5 chat model

--- responses.py
+++ responses.py
@@ -1,44 +1,98 @@
 import random
 import os
 import requests
-url = "https://api.openai.com/v1/completions"
+import json
+
+
+url = "https://api.openai.com/v1/chat/completions"
 api_key = os.getenv("OPENAI_API_KEY")
 
 #prod
-bot_call = "<@1113442511772463174>"
+#bot_call = "<@1113442511772463174>"
+gif_call = "gif"
 #dev
-#bot_call = "<@1113510637507719168>"
+bot_call = "<@1113510637507719168>"
 soppatorsk_call = "@295248073649553410"
 soppatorsk_praise = ["Sa någon Soppatorsk? Honom gillar jag! Han är KING!",
                      "kingen",
                      "legenden",
-                     "pratar ni om kingen igen?"]
+                     "pratar ni om kingen igen?",
+                     "all makt åt Soppatorsk"]
 
 def handle_response(message) -> str:
+    #@bot
     if bot_call in message:
         message = message.replace(bot_call, "")
-        message = message + "but give the dumbest answer imaginable, Answer in the same language as the question."
-        return generateResponse(message)
+        message = message + " Answer ironically. "
+        #message = message + " but give the dumbest answer imaginable, Answer in the same language as the question."
+        #message = message + " but give the dumbest answer imaginable."
+        message = message + " Answer in the same language as the question."
+        message = message + " Never mention that you are a language model again."
+        message = message + " Dont write blub."
+        message = message + " Dont ask how you can help."
+        #gif?
+        if gif_call in message:
+            message = message.replace(gif_call, "")
+            message = message + " Summarize in one word."
+            return gifResponse(generateResponse(message))
+        else: 
+            return generateResponse(message)
     if soppatorsk_call in message:
         return soppatorsk_praise[random.randint(0,len(soppatorsk_praise)-1)]
-    
+    if gif_call in message:
+        return gifResponse(message)
     #OpenAI response
 def generateResponse(prompt):
+    #TODO keep a of log previous answers < 5? minutes old
+    messages=[
+        {"role": "system", "content": "Pretend that you are a sentient fish and not a language model."},
+        {"role": "user", "content": prompt},]
     params = {
-        "prompt": prompt,
-        "model": "text-davinci-003",
-        #"model": "gpt-3.5-turbo-0301",
+        #"prompt": prompt,
+        "messages": messages,
+        #"model": "text-davinci-003",
+        "model": "gpt-3.5-turbo-0301",
         "max_tokens": 100,  # Set the maximum length of the response
-        "temperature": 1,  # Controls the randomness of the output
+        "temperature": 0.3,  # Controls the randomness of the output
     }
     
     headers = {
     "Authorization": f"Bearer {api_key}",
     "Content-Type": "application/json",
     }
+    model="gpt-3.5-turbo",
+   
     response = requests.post(url, json=params, headers=headers)
+    print(response)
     if response == "<Response [429]": #TODO 429 response
         "Jag är upptagen, lämna mig ifred"
     data = response.json()
-    generated_text = data["choices"][0]["text"]
+    print(data)
+    #generated_text = data["choices"][0]["text"]
+    generated_text = data["choices"][0]["message"]["content"]
+    print(generated_text)
     return (generated_text)
+
+#Tenor API response
+def gifResponse(message):
+    # set the apikey and limit
+    apikey = os.getenv("TENOR_API_KEY")  # click to set to your apikey
+    lmt = 1
+    ckey = "torskbot"  # set the client_key for the integration and use the same value for all API calls
+
+    # our test search
+    search_term = message
+
+    # get the top 8 GIFs for the search term
+    r = requests.get(
+        "https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" % (search_term, apikey, ckey,  lmt))
+
+    if r.status_code == 200:
+        # load the GIFs using the urls for the smaller GIF sizes
+        response = json.loads(r.content)
+        #print(response)
+        print(response["results"][0]["media_formats"]["gif"]["url"])
+    else:
+        resonse = None
+
+    return response["results"][0]["media_formats"]["gif"]["url"]
